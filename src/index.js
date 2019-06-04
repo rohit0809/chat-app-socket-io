ebfaf8f54aea9eca5d842e534c03e51a3aa73a42
Rohit Anand <rohit.aanand1005@gmail.com>
2019-06-05 00:02:41 +0530

    Socket io Events

--- src/index.js
+++ src/index.js
@@ -12,8 +12,21 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+let count = 0
+
+// server (emit) -> client (receive) -- countUpdated
+// client (emit) -> server (receive) -- increment
+
+io.on('connection', (socket) => {
     console.log('New Websocket connection!!')
+
+    socket.emit('countUpdated',count)
+
+    socket.on('increment', () => {
+        count++
+        // socket.emit('countUpdated', count) -- emits to single connection
+        io.emit('countUpdated', count) // emits to all client connections
+    })
 })
 
 server.listen(port, () => {
